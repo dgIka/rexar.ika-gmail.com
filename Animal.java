9633a004f66426e782a0a590cc2e955a04176b43
Irakliy <rexar.ika@gmail.com>
2024-01-05 02:36:37 +0300

    First test

---
+++ Animal.java
@@ -0,0 +1,22 @@
+package chatgptquests.secondTask;
+
+public class Animal {
+    private String name;
+    private String species;
+
+    public Animal(String name, String species) {
+        this.name = name;
+        this.species = species;
+    }
+
+    public String getName(){
+        return this.name;
+    }
+
+    public String getSpecies(){
+        return this.species;
+    }
+
+
+
+}
